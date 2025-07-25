88b38ca4f7a1f79bff978bb1b47f5e80b509733b
Jelmerrr <Jelljel@hotmail.com>
2025-07-25 09:06:11 +0200

    Small Utils cleanup

--- Scripts/Globals/Utils.gd
+++ Scripts/Globals/Utils.gd
@@ -1,12 +1,5 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("DebugExit"):
+func _process(_delta: float) -> void:
+	if Input.is_action_pressed("DebugExit"): #Placeholder voor debugging, haal dit weg voor build.
 		get_tree().quit()
