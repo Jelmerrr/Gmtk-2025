e007b6558a87fe9e7d277d7d337c697c1fa302cd
Jelmerrr <Jelljel@hotmail.com>
2025-07-31 18:56:24 +0200

    Level Handler

---
+++ Scripts/Globals/Level_Library.gd
@@ -0,0 +1,3 @@
+extends Node
+
+@onready var level1 = preload("res://Scenes/Levels/level_1.tscn")
--- Scripts/Level/Game_Handler.gd
+++ Scripts/Level/Game_Handler.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
+@onready var level_instance: Node2D = $Level_Instance
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	InstantiateLevel(LevelLibrary.level1)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func InstantiateLevel(level):
+	var levelInstance = level.instantiate()
+	level_instance.add_child.call_deferred(levelInstance)
