59657fd087eff3cd3371468e93f8199b74289d3a
Jelmerrr <Jelljel@hotmail.com>
2025-08-01 14:58:38 +0200

    Menu controller update

--- Scripts/UI/Menu_Controller.gd
+++ Scripts/UI/Menu_Controller.gd
@@ -1,11 +1,13 @@
 extends Control
 
+#TODO: Options?
+
 func _on_start_button_pressed() -> void:
 	SceneHandler.switch_scene(SceneHandler.level_path)
 
 func _on_level_select_button_pressed() -> void:
+	#TODO: Hier nog iets mee doen?
 	pass # Replace with function body.
-
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
