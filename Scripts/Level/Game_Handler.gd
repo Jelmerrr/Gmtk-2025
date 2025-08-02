50b346136e46fd834c5b595fafe50699cb4b32fe
Jelmerrr <Jelljel@hotmail.com>
2025-08-02 02:06:46 +0200

    Added basic scene transitions

--- Scripts/Level/Game_Handler.gd
+++ Scripts/Level/Game_Handler.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
 @onready var level_instance: Node2D = $Level_Instance
+@onready var scene_transition: Node2D = $Camera2D/CanvasLayer/SceneTransition
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	scene_transition.get_node("ColorRect").color.a = 255
+	scene_transition.FadeOut()
 	InstantiateLevel(LevelLibrary.level1)
 
 func InstantiateLevel(level):
