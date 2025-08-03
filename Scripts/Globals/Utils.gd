5a32930b9d272a950b6ba6dc9f5eb6ce1f4377c3
Jelmerrr <Jelljel@hotmail.com>
2025-08-03 19:54:59 +0200

    final changes

--- Scripts/Globals/Utils.gd
+++ Scripts/Globals/Utils.gd
@@ -7,4 +7,5 @@ enum playerSurfaceState{
 
 func _process(_delta: float) -> void:
 	if Input.is_action_pressed("DebugExit"): #Placeholder voor debugging, haal dit weg voor build.
-		get_tree().quit()
+		pass
+		#get_tree().quit()
